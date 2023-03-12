05d649438403e37188e015b93a788d75236f88bc
mertwole <mertwole@gmail.com>
2023-03-12 10:15:44 +0300

    RoomBase prefab

---
+++ JamGame/Assets/Scripts/Room/WallCollection.cs
@@ -0,0 +1,40 @@
+using Common;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct WallCollectionEntry
+{
+    public WallType type;
+    public GameObject wall;
+}
+
+public class WallCollection : MonoBehaviour
+{
+    [SerializeField] WallCollectionEntry[] walls;
+    Dictionary<WallType, GameObject> wallsDict;
+    GameObject activeWall;
+
+    void Start()
+    {
+        wallsDict = new Dictionary<WallType, GameObject>();
+        foreach (var wall in walls)
+            if (wall.wall != null)
+                wallsDict.Add(wall.type, wall.wall);
+    }
+
+    public IEnumerator<WallType> GetAvailableWalls()
+    {
+        return wallsDict.Keys.GetEnumerator();
+    }
+
+    public void SetWall(WallType type)
+    {
+        if (activeWall != null)
+            activeWall.SetActive(true);
+
+        activeWall = wallsDict[type].gameObject;
+        activeWall.SetActive(true);
+    }
+}
