290d2c8a7a8424ad9aa7977a9067ebb9c17ad6cd
mertwole <mertwole@gmail.com>
2023-03-12 14:58:31 +0300

    Add Corridor prefab

--- JamGame/Assets/Scripts/Common/RoomType.cs
+++ JamGame/Assets/Scripts/Common/RoomType.cs
@@ -3,11 +3,10 @@
     public enum RoomType
     {
         // Add room types
-        Empty,
+        Empty, // Empty room (with floor and walls but without stuff)
         Corridor,
         Outside, // Build forbidden
         None, // No room haven't built
+        WorkPlace
     }
-
-    
 }
