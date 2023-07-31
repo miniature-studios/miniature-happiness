42ded4f8082865d25d0b59fa75288b57b63143ea
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-07-31 13:33:44 +0300

    Add config handler

---
+++ JamGame/Assets/Scripts/Level/ConfigHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    [AddComponentMenu("Level.ConfigHandler")]
+    public class ConfigHandler : MonoBehaviour
+    {
+        public Config.LevelConfig Config;
+    }
+}
