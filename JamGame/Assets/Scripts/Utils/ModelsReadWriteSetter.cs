45a8d0914c4dae3ec31bad89094b990bdcf0d2c5
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-08-02 15:11:16 +0300

    Dynamic navmesh generation (#22)
    * NavMesh Dynamic Generation
    
    * Adding Models ReadWrite Setter
    
    * Namespace fix
    
    * Nameing Fix
    
    * Nameing Fix

---
+++ JamGame/Assets/Scripts/Utils/ModelsReadWriteSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utils
+{
+    public class ModelsReadWriteFlagSetter
+    {
+        [MenuItem("Tools/ModelsReadWriteSetter/Set Models read|write flag")]
+        private static void Hierarchy()
+        {
+            IEnumerable<string> files = Directory
+                .GetFiles("Assets", "*.*", SearchOption.AllDirectories)
+                .Where(x => x.EndsWith("obj") || x.EndsWith("fbx"));
+
+            foreach (string path in files)
+            {
+                ModelImporter model_importer = (ModelImporter)AssetImporter.GetAtPath(path);
+                model_importer.isReadable = true;
+                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            }
+
+            Debug.Log("Hierarchy animators setup done");
+        }
+    }
+}
