4c55b99b7c1205b47e80f8a225113c8c5269ba15
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-08-02 13:42:45 +0300

    Adding Models ReadWrite Setter

---
+++ JamGame/Assets/Scripts/Utils/ModelsReadWriteSetterTools.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utils
+{
+    public class ModelsReadWriteSetterTools
+    {
+        [MenuItem("Tools/ModelsReadWriteSetter/Set Models Read|Write flag in Assets on true")]
+        private static void Hierarchy()
+        {
+            IEnumerable<string> files = Directory
+                .GetFiles("Assets", "*.*", SearchOption.AllDirectories)
+                .Where(x => x.EndsWith("obj") || x.EndsWith("fbx"));
+
+            foreach (string path in files)
+            {
+                ModelImporter A = (ModelImporter)AssetImporter.GetAtPath(path);
+                A.isReadable = true;
+                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            }
+
+            Debug.Log("Assets models setup done");
+        }
+    }
+}
