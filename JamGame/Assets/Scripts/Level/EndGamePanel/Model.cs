72cfcb3e9f3d4e081c1de01cbb20bea5d1cf7446
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-09-02 14:42:10 +0300

    Adding Game End Screen

---
+++ JamGame/Assets/Scripts/Level/EndGamePanel/Model.cs
@@ -0,0 +1,50 @@
+﻿using Common;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Level.EndGamePanel
+{
+    [AddComponentMenu("Scripts/Level.EndGamePanel.Model")]
+    public class Model : MonoBehaviour
+    {
+        [SerializeField]
+        [InspectorReadOnly]
+        private string panelText;
+
+        [SerializeField]
+        [InspectorReadOnly]
+        private bool shown;
+
+        [SerializeField]
+        private string loadingScene;
+
+        public UnityEvent<string> TextChange;
+        public UnityEvent<bool> ShownTrigger;
+
+        public string PanelText
+        {
+            get => panelText;
+            set
+            {
+                panelText = value;
+                TextChange?.Invoke(panelText);
+            }
+        }
+
+        public bool Shown
+        {
+            get => shown;
+            set
+            {
+                shown = value;
+                ShownTrigger?.Invoke(shown);
+            }
+        }
+
+        public void TryAgainClick()
+        {
+            SceneManager.LoadScene(loadingScene);
+        }
+    }
+}
