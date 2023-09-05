80153372a804597e2c1bfb176a045231bf0af926
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-09-06 00:37:25 +0300

    Repair for working

--- JamGame/Assets/Scripts/Level/Executor.cs
+++ JamGame/Assets/Scripts/Level/Executor.cs
@@ -100,7 +100,7 @@ namespace Level
         {
             location.InitGameMode();
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 0; i++)
             {
                 location.AddEmployee(TestEmployeeConfig.ToEmployeeConfig().GetEmployeeConfig());
             }
