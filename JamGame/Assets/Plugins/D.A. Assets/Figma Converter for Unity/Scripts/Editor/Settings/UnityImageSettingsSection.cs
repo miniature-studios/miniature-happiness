eae87c65ca277a09856af3ff41fb8078b05340b6
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-05-06 18:16:25 +0300

    Complete merge

--- JamGame/Assets/Plugins/D.A. Assets/Figma Converter for Unity/Scripts/Editor/Settings/UnityImageSettingsSection.cs
+++ JamGame/Assets/Plugins/D.A. Assets/Figma Converter for Unity/Scripts/Editor/Settings/UnityImageSettingsSection.cs
@@ -1,15 +1,15 @@
-﻿using DA_Assets.FCU.Extensions;
+﻿using System.Linq;
+using DA_Assets.FCU.Extensions;
 using DA_Assets.Shared;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace DA_Assets.FCU
 {
-    internal class UnityImageSettingsSection : ScriptableObjectBinder<FcuSettingsWindow, FigmaConverterUnity>
+    internal class UnityImageSettingsSection
+        : ScriptableObjectBinder<FcuSettingsWindow, FigmaConverterUnity>
     {
         private string[] shaderNames;
-        private int selectedIndex = -1;
 
         public override void Init()
         {
@@ -18,29 +18,39 @@ namespace DA_Assets.FCU
 
         public void Draw()
         {
-            gui.SectionHeader(FcuLocKey.label_unity_image_settings.Localize());
+            _ = gui.SectionHeader(FcuLocKey.label_unity_image_settings.Localize());
             gui.Space15();
 
             if (monoBeh.UsingRawImage() == false)
             {
-                monoBeh.Settings.UnityImageSettings.Type = gui.EnumField(new GUIContent(FcuLocKey.label_image_type.Localize(), ""),
-                    monoBeh.Settings.UnityImageSettings.Type);
+                monoBeh.Settings.UnityImageSettings.Type = gui.EnumField(
+                    new GUIContent(FcuLocKey.label_image_type.Localize(), ""),
+                    monoBeh.Settings.UnityImageSettings.Type
+                );
             }
 
-            monoBeh.Settings.UnityImageSettings.RaycastTarget = gui.Toggle(new GUIContent(FcuLocKey.label_raycast_target.Localize(), ""),
-                monoBeh.Settings.UnityImageSettings.RaycastTarget);
+            monoBeh.Settings.UnityImageSettings.RaycastTarget = gui.Toggle(
+                new GUIContent(FcuLocKey.label_raycast_target.Localize(), ""),
+                monoBeh.Settings.UnityImageSettings.RaycastTarget
+            );
 
             if (monoBeh.UsingRawImage() == false)
             {
-                monoBeh.Settings.UnityImageSettings.PreserveAspect = gui.Toggle(new GUIContent(FcuLocKey.label_preserve_aspect.Localize(), ""),
-                    monoBeh.Settings.UnityImageSettings.PreserveAspect);
+                monoBeh.Settings.UnityImageSettings.PreserveAspect = gui.Toggle(
+                    new GUIContent(FcuLocKey.label_preserve_aspect.Localize(), ""),
+                    monoBeh.Settings.UnityImageSettings.PreserveAspect
+                );
             }
 
-            monoBeh.Settings.UnityImageSettings.RaycastPadding = gui.Vector4Field(new GUIContent(FcuLocKey.label_raycast_padding.Localize(), ""),
-                monoBeh.Settings.UnityImageSettings.RaycastPadding);
+            monoBeh.Settings.UnityImageSettings.RaycastPadding = gui.Vector4Field(
+                new GUIContent(FcuLocKey.label_raycast_padding.Localize(), ""),
+                monoBeh.Settings.UnityImageSettings.RaycastPadding
+            );
 
-            monoBeh.Settings.UnityImageSettings.Maskable = gui.Toggle(new GUIContent(FcuLocKey.label_maskable.Localize(), ""),
-                monoBeh.Settings.UnityImageSettings.Maskable);
+            monoBeh.Settings.UnityImageSettings.Maskable = gui.Toggle(
+                new GUIContent(FcuLocKey.label_maskable.Localize(), ""),
+                monoBeh.Settings.UnityImageSettings.Maskable
+            );
         }
     }
 }
