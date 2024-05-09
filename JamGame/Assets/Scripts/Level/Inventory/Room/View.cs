c6e7bc57e8060844d832be87295dd2609fe9cea8
mertwole <33563701+mertwole@users.noreply.github.com>
2024-05-10 02:07:53 +0400

    New inventory UI (#180)

--- JamGame/Assets/Scripts/Level/Inventory/Room/View.cs
+++ JamGame/Assets/Scripts/Level/Inventory/Room/View.cs
@@ -1,37 +1,39 @@
 using System.Collections.Generic;
+using System.Linq;
 using Common;
 using Level.Room;
-using Pickle;
 using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Level.Inventory.Room
 {
     [AddComponentMenu("Scripts/Level/Inventory/Room/Level.Inventory.Room.View")]
     public class View : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler, IUidHandle
     {
-        [RequiredIn(PrefabKind.Variant | PrefabKind.InstanceInScene)]
-        [Pickle(LookupType = ObjectProviderType.Assets)]
-        public CoreModel CoreModelPrefab;
-        public InternalUid Uid => CoreModelPrefab.Uid;
-
-        [Required]
-        [SerializeField]
-        private ExtendedView extendedView;
-
         [Required]
         [SerializeField]
         private TMP_Text countLabel;
 
+        [Required]
+        [SerializeField]
+        private Image miniature;
+
         [ReadOnly]
         [SerializeField]
         private List<CoreModel> coreModels = new();
+
+        public InternalUid Uid => coreModels.FirstOrDefault()?.Uid;
+        public CoreModel CoreModel => coreModels.FirstOrDefault();
+
         public bool IsEmpty => coreModels.Count == 0;
 
-        private bool isHovered = false;
         private InputActions inputActions;
+
+        public delegate void HoverChangedHandler(View sender, bool hovered);
+        public event HoverChangedHandler OnHoverChanged;
 
         private void Awake()
         {
@@ -50,40 +52,48 @@ namespace Level.Inventory.Room
 
         public void AddCoreModel(CoreModel coreModel)
         {
+            if (!IsEmpty && coreModel.Uid != Uid)
+            {
+                Debug.LogError(
+                    $"Trying to add room with wrong Uid. Expected: {Uid}, got: {coreModel.Uid}"
+                );
+                return;
+            }
+
             coreModels.Add(coreModel);
             coreModel.transform.SetParent(transform);
+
+            UpdateData();
         }
 
         public void RemoveCoreModel(CoreModel coreModel)
         {
             _ = coreModels.Remove(coreModel);
+            if (!IsEmpty)
+            {
+                UpdateData();
+            }
         }
 
-        public void Update()
+        public void RemoveAllCoreModels()
         {
+            coreModels.Clear();
+        }
+
+        private void UpdateData()
+        {
+            miniature.sprite = coreModels.First().InventoryModel.Miniature;
             countLabel.text = coreModels.Count.ToString();
-            if (inputActions.UI.ExtendInventoryTileInfo.IsPressed() && isHovered)
-            {
-                if (!extendedView.IsVisible)
-                {
-                    extendedView.Show();
-                    extendedView.SetLabelText($"Rent: {CoreModelPrefab.RoomInfo.RentCost.Value}");
-                }
-            }
-            else if (extendedView.IsVisible)
-            {
-                extendedView.Hide();
-            }
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            isHovered = true;
+            OnHoverChanged?.Invoke(this, true);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            isHovered = false;
+            OnHoverChanged?.Invoke(this, false);
         }
     }
 }
