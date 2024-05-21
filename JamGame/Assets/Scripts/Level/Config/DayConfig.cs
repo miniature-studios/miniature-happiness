6b5a9c8291b75a330bb1faa440db390d9badbab9
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-05-21 20:15:17 +0300

    Add SO day config

--- JamGame/Assets/Scripts/Level/Config/DayConfig.cs
+++ JamGame/Assets/Scripts/Level/Config/DayConfig.cs
@@ -5,12 +5,39 @@ using UnityEngine;
 
 namespace Level.Config
 {
+    public enum ConfigMode
+    {
+        Raw,
+        ConfigLink
+    }
+
     [Serializable]
     [HideReferenceObjectPicker]
     public class DayConfig
     {
+        [HideLabel]
+        [SerializeField]
+        [EnumToggleButtons]
+        private ConfigMode configMode = ConfigMode.Raw;
+
+        [ShowIf(nameof(configMode), ConfigMode.Raw)]
         [SerializeReference]
         private List<IDayAction> dayActions = new();
-        public IEnumerable<IDayAction> DayActions => dayActions;
+
+        [ShowIf(nameof(configMode), ConfigMode.ConfigLink)]
+        [SerializeField]
+        private DayConfigBundle bundle;
+
+        public IEnumerable<IDayAction> DayActions => GetDayActions();
+
+        private IEnumerable<IDayAction> GetDayActions()
+        {
+            return configMode switch
+            {
+                ConfigMode.Raw => dayActions,
+                ConfigMode.ConfigLink => bundle.DayActions,
+                _ => throw new ArgumentException()
+            };
+        }
     }
 }
---
+++ JamGame/Assets/Scripts/Level/Config/DayConfigBundle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Level.Config
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "DayConfigBundle", menuName = "Level/DayConfigBundle")]
+    internal class DayConfigBundle : ScriptableObject
+    {
+        [SerializeField]
+        [InlineProperty]
+        [HideLabel]
+        private DayConfig dayConfig = new();
+        public IEnumerable<IDayAction> DayActions => dayConfig.DayActions;
+    }
+}
