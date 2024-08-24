ac87d073f6f500a5d7225dbd657508cbafee14c2
mertwole <mertwole@gmail.com>
2024-08-24 13:43:40 +0400

    Remove Days struct

--- JamGame/Assets/Scripts/Level/GlobalTime/TimeMeasures.cs
+++ JamGame/Assets/Scripts/Level/GlobalTime/TimeMeasures.cs
@@ -6,69 +6,6 @@ namespace Level.GlobalTime
 {
     [Serializable]
     [InlineProperty]
-    public struct Days
-    {
-        [SerializeField]
-        private float days;
-        public readonly float Value => days;
-
-        public readonly RealTimeSeconds RealTimeSeconds => new(days * Model.DayLength.Value);
-
-        public static Days Zero => new(0);
-
-        public Days(RealTimeSeconds seconds)
-        {
-            days = seconds.Value / Model.DayLength.Value;
-        }
-
-        public Days(float days)
-        {
-            this.days = days;
-        }
-
-        public static Days operator +(Days a, Days b)
-        {
-            return new Days() { days = a.days + b.days };
-        }
-
-        public static Days operator -(Days a, Days b)
-        {
-            return new Days() { days = a.days - b.days };
-        }
-
-        public static Days operator *(Days a, float b)
-        {
-            return new Days() { days = a.days * b };
-        }
-
-        public static Days operator *(float a, Days b)
-        {
-            return new Days() { days = a * b.days };
-        }
-
-        public static bool operator >(Days a, Days b)
-        {
-            return a.days > b.days;
-        }
-
-        public static bool operator <(Days a, Days b)
-        {
-            return a.days < b.days;
-        }
-
-        public static bool operator <=(Days a, Days b)
-        {
-            return a.days <= b.days;
-        }
-
-        public static bool operator >=(Days a, Days b)
-        {
-            return a.days >= b.days;
-        }
-    }
-
-    [Serializable]
-    [InlineProperty]
     public struct RealTimeSeconds
     {
         [SerializeField]
