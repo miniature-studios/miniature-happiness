ad5c586172d9cf9055810fff7add910e5b9a888b
mertwole <33563701+mertwole@users.noreply.github.com>
2024-10-04 14:49:43 +0400

    Remove `overlay` system (#203)

--- JamGame/Assets/Scripts/Employee/View.cs
+++ JamGame/Assets/Scripts/Employee/View.cs
@@ -1,4 +1,3 @@
-using Overlay;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AI;
@@ -29,88 +28,10 @@ namespace Employee
 
         private void Update()
         {
-            UpdateStressOverlay();
             animator.SetFloat(
                 "WalkSpeed",
                 agent.velocity.magnitude / agent.speed * animationSpeedMultiplier
             );
-        }
-
-        public void RevertOverlays()
-        {
-            RevertStressOverlay();
-            RevertExtendedInfoOverlay();
-        }
-    }
-
-    [RequireComponent(typeof(PostEffectMaskRenderer))]
-    public partial class View : IOverlayRenderer<Stress>
-    {
-        [Required]
-        [SerializeField]
-        private SkinnedMeshRenderer meshRenderer;
-
-        [Required]
-        [SerializeField]
-        private Material baseMaterial;
-
-        [Required]
-        [SerializeField]
-        private Material stressMaterial;
-
-        private Stress appliedStressOverlay;
-
-        private PostEffectMaskRenderer maskRenderer;
-
-        public void ApplyOverlay(Stress overlay)
-        {
-            if (maskRenderer == null)
-            {
-                maskRenderer = GetComponent<PostEffectMaskRenderer>();
-                PostEffectMask mask = Camera.main.GetComponent<PostEffectMask>();
-                if (mask == null)
-                {
-                    Debug.LogError("Unable to find PostEffectMask on main camera");
-                }
-                maskRenderer.mask = mask;
-            }
-
-            appliedStressOverlay = overlay;
-            meshRenderer.material = stressMaterial;
-        }
-
-        private void UpdateStressOverlay()
-        {
-            if (appliedStressOverlay == null)
-            {
-                return;
-            }
-
-            meshRenderer.material.color = appliedStressOverlay.GetCurrentColor(employee.Stress);
-        }
-
-        public void RevertStressOverlay()
-        {
-            appliedStressOverlay = null;
-            meshRenderer.material = baseMaterial;
-        }
-    }
-
-    public partial class View : IOverlayRenderer<ExtendedEmployeeInfo>
-    {
-        [SerializeField]
-        [Required]
-        [ChildGameObjectsOnly]
-        private ExtendedInfo.View extendedInfoView;
-
-        public void ApplyOverlay(ExtendedEmployeeInfo overlay)
-        {
-            extendedInfoView.gameObject.SetActive(true);
-        }
-
-        public void RevertExtendedInfoOverlay()
-        {
-            extendedInfoView.gameObject.SetActive(false);
         }
     }
 }
